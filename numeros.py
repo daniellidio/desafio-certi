1a5d1b00a44e65e9ac1145681db1d48604a9dc5a
Daniel Lidio <daniellidiops@gmail.com>
2020-02-19 07:38:56 -0300

    Mais alguns passos da tradução foram adicionados.

--- numeros.py
+++ numeros.py
@@ -17,10 +17,6 @@ centenas = [
 
 cem = ['cem']
 
-mil = ['mil']
-
-ddd = '200'
-
 def numero(num):
     if num < 20:
         return um_a_dezenove[num]
@@ -37,7 +33,16 @@ def numero(num):
             return centenas[num // 100 - 1] + ' e ' + dezenas[(num - (int(str(num)[:1]) * 100)) // 10 - 2] + \
             ' e ' + um_a_dezenove[num - ((int(str(num)[:2])) * 10)]
     elif num == 1000:
-        return mil[0]
+        return 'mil'
+    elif num < 100000:
+        if len(str(num)) == 4 and str(num)[1:4] == '000':
+            return um_a_dezenove[num // 1000] + ' mil'
+        elif len(str(num)) == 4 and str(num)[2:4] == '00':
+            return um_a_dezenove[num // 1000] + ' mil e ' + centenas[(num - (int(str(num)[:1]) * 1000)) // 100 - 1]
+        elif len(str(num)) == 4 and str(num)[1:2] == '0' and str(num)[2:3] != '0' and str(num)[3:4] == '0':
+            return um_a_dezenove[num // 1000] + ' mil e ' + dezenas[(num - (int(str(num)[:1]) * 1000)) // 10 - 2]
+        elif len(str(num)) == 4 and str(num)[1:3] == '00':
+            return um_a_dezenove[num // 1000] + ' mil e ' + um_a_dezenove[(num - (int(str(num)[:1]) * 1000))]
     else:
         return 'Escreva um número de 0 a 1000'
 
@@ -50,4 +55,12 @@ print(numero(200))
 print(numero(220))
 print(numero(221))
 print(numero(999))
-print(numero(1000))
+print(numero(1020))
+print(numero(2040))
+print(numero(2000))
+print(numero(2200))
+print(numero(2001))
+print('1235'[1:2])
+print('1235'[2:3])
+print('1235'[3:4])
+print('1235'[2:4])
